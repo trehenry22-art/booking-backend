3ea7bbd5f24eff7ffcc83bbc87cd28d21a2b8998
trehenry22-art <trehenry22@gmail.com>
2026-03-12 09:41:01 -0400

    fix render port

--- server.js
+++ server.js
@@ -82,6 +82,8 @@ app.post("/create-checkout-session", async (req, res) => {
   }
 });
 
-app.listen(3000, () => {
-  console.log("Server running on port 3000");
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
 });
